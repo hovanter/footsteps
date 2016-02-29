69f7ff998e0608a992c0d61c63883aea08374602
Catherine Mullings <catherinemullings@DN0a2337bf.SUNet>
2016-02-28 17:47:37 -0800

    Button changes color on record

--- footsteps/js/main.js
+++ footsteps/js/main.js
@@ -24,11 +24,17 @@ $(document).ready(function() {
   $('.slide-group').slick({dots: true});
   $('.slide-group button').remove(); 
 
-  $('#recording-icon').click(function() {
-  	if($('.filter-bar').hasClass('slide-up')) {
-  		
-  	} else {
+  var recordButton = $('.action-container:first-child .action-button');
 
+  recordButton.click(function() {
+  	if(recordButton.hasClass('icon-stop')) {
+  	 	recordButton.removeClass('icon-stop');
+  		recordButton.addClass('icon-play');
+  		$('.action-container:first-child').removeAttr('id');
+  	} else {
+  	 	recordButton.removeClass('icon-play');
+  		recordButton.addClass('icon-stop'); 
+		$('.action-container:first-child').attr('id', 'isRecording'); 	
   	}
   });
 
