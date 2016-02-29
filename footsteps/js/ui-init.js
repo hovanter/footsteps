8edf710281717a5d61d64f25a8b32ceed3520190
Ben-han Sung <bhnascar@Ben-hans-Air.lan>
2016-02-29 03:59:21 -0800

    Some more organization things, AJAX support for contents of sidebar.

--- footsteps/js/ui-init.js
+++ footsteps/js/ui-init.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-    /* Wire up interface button actions. */
+    // Wire up chevron to show/hide filter menu.
     $('.filter-icon').click(function() {
-        setSidebarContent('#filters')
 		if (isSidebarVisible()) {
             $('.filter-icon').removeClass('icon-down-nav');
             $('.filter-icon').addClass('icon-up-nav');
             $('.filter-container').removeClass('slide-up-container', 1000, 'easeOutExpo'); 
 	 		hideSidebar();
 	 	} else {
+            setSidebarContent('filters.html');
             $('.filter-icon').removeClass('icon-up-nav');
             $('.filter-icon').addClass('icon-down-nav');
             $('.filter-container').addClass('slide-up-container', 1000, 'easeOutExpo');
@@ -16,22 +16,21 @@ $(document).ready(function() {
 	 	}
     });
 
-    /* Slack init scripts. */
+    // Wire up record button to switch between play/stop icons.
+    var recordButton = $('.action-container:first-child .action-button');
+    recordButton.click(function() {
+        if(recordButton.hasClass('icon-stop')) {
+            recordButton.removeClass('icon-stop');
+            recordButton.addClass('icon-play');
+            $('.action-container:first-child').removeAttr('id');
+        } else {
+            recordButton.removeClass('icon-play');
+            recordButton.addClass('icon-stop'); 
+        $('.action-container:first-child').attr('id', 'isRecording');   
+        }
+    });
+
+    // Slick init scripts.
     $('.slide-group').slick({dots: true});
     $('.slide-group button').remove(); 
-
-    var recordButton = $('.action-container:first-child .action-button');
-
-    recordButton.click(function() {
-    	if(recordButton.hasClass('icon-stop')) {
-    	 	recordButton.removeClass('icon-stop');
-    		recordButton.addClass('icon-play');
-    		$('.action-container:first-child').removeAttr('id');
-    	} else {
-    	 	recordButton.removeClass('icon-play');
-    		recordButton.addClass('icon-stop'); 
-    	$('.action-container:first-child').attr('id', 'isRecording'); 	
-    	}
-    });
-    
 });
--- footsteps/js/ui-utils.js
+++ footsteps/js/ui-utils.js
@@ -1,30 +1,55 @@
+/**** Popup implementation ****/
+
+// Hides the popup.
+function hidePopup() {
+    $('#popup').height(0);
+}
+
+// Shows the popup.
+function showPopup(height) {
+    $('#popup').height(height);
+}
+
+// Returns whether or not the popup is currently visible.
+function isPopupVisible() {
+    return $('#popup').height() > 0;
+}
+
+// Sets the content of the popup with the content of the
+// given HTML file. Uses an AJAX call.
+function setPopupContent(html_file) {
+    $('#popup').load(html_file);
+}
+
+/**** Sidebar implementation ****/
+
 // Hides the sidebar.
 function hideSidebar() {
-	/*
-    $('.sidebar').addClass('slide-down', 1000, 'easeOutExpo');
-    $('.sidebar').removeClass('slide-up', 1000, 'easeOutExpo');
-    */
-    $('.sidebar').height(0);
+    $('#sidebar').height(0);
 }
 
 // Shows the sidebar.
 function showSidebar(height) {
-	/*
-    $('.sidebar').addClass('slide-up', 1000, 'easeOutExpo');
-    $('.sidebar').removeClass('slide-down', 1000, 'easeOutExpo');
-    */
-    $('.sidebar').height(height);
+    $('#sidebar').height(height);
 }
 
 // Returns whether or not the sidebar is currently visible.
 function isSidebarVisible() {
-	return $('.sidebar').height() > 0;
+	return $('#sidebar').height() > 0;
 }
 
+// Sets the content of the sidebar with the content of the
+// given HTML file. Uses an AJAX call.
+function setSidebarContent(html_file) {
+    $('#sidebar').load(html_file);
+}
+
+/*
 // cssSelector indicates the CSS ID of the sidebar content
 // that you would like to display. All other content is
 // hidden.
 function setSidebarContent(cssSelector) {
-    $('.sidebar-content').not('.sidebar ' + cssSelector).hide();
-    $('.sidebar ' + cssSelector).show();
+    $('#sidebar-content').not('#sidebar ' + cssSelector).hide();
+    $('#sidebar ' + cssSelector).show();
 }
+*/
