38b6d61c38f6364dbd55509c0f19952a341996c1
Catherine Mullings <catherinemullings@DN0a22555e.SUNet>
2016-03-01 13:07:16 -0800

    Toggle record button

--- footsteps/js/ui-init.js
+++ footsteps/js/ui-init.js
@@ -10,24 +10,32 @@ $(document).ready(function() {
 	 	}
     });
 
-    // Wire up record button to switch between play/stop icons.
-    var recordButton = $('.action-container:first-child .action-button');
-    recordButton.click(function() {
-        if(recordButton.hasClass('icon-stop')) {
-            recordButton.removeClass('icon-stop');
-            recordButton.addClass('icon-play');
-            $('.action-container:first-child').removeAttr('id');
-        } else {
-            recordButton.removeClass('icon-play');
-            recordButton.addClass('icon-stop'); 
-        $('.action-container:first-child').attr('id', 'isRecording');   
-        }
-    });
-
+    // Wire up drop button to show marker menu popup.
     var dropButton = $('.action-container:nth-child(2)');
     dropButton.click(function() {
         setPopupContent('drop-options.html')
         showPopup(70);
+    });
+
+    // Wire up record button to switch between play/stop icons.
+    var recordButton = $('.action-container:first-child .action-button');
+    recordButton.click(function() {
+
+        dropButton.toggle();
+
+        if(recordButton.hasClass('icon-stop')) {
+            recordButton.removeClass('icon-stop');
+            recordButton.addClass('icon-play');
+            $('.action-container:first-child').removeAttr('id');
+
+            // dropButton.addClass('');
+        } else {
+            recordButton.removeClass('icon-play');
+            recordButton.addClass('icon-stop'); 
+            $('.action-container:first-child').attr('id', 'isRecording');
+
+            // dropButton.removeClass('');   
+        }
     });
 
     // Slick init scripts.
